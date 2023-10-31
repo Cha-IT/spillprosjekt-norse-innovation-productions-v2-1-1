124ddb9b0242fa3b5cb35d7c46819aac2fb2a32d
Locopelle <113931913+Locopelle@users.noreply.github.com>
2023-10-31 09:41:26 +0100

    Started on maze and added md file

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-# Importerer forskjellige libraries inn i vårt spill
+''' Importerer forskjellige libraries inn i vårt spill '''
 import pygame
 from pygame.locals import (
-    QUIT,
-    KEYDOWN
+    K_ESCAPE,
+    K_DOWN,
+    K_UP,
+    K_RIGHT,
+    K_LEFT,
+    KEYDOWN,
+    QUIT
 )
 import random
+import os
 
-# Initialiserer Pygame som Library og funksjon
+''' Initialiserer Pygame som Library og funksjon '''
 pygame.init()
 
 # Vindu Høyde og bredde
-WINDOW_HEIGHT = 850
-WINDOWS_WIDTH = 850
+WINDOW_HEIGHT = 650
+WINDOWS_WIDTH = 650
 
 # Fargepalett
 COLOR_WHITE = (255, 255, 255)
@@ -21,10 +27,14 @@ COLOR_YELLOW = (253, 255, 0)
 
 screen = pygame.display.set_mode([WINDOWS_WIDTH, WINDOW_HEIGHT])
 
+''' CLASSES '''
+
+# Spiller classen
 class PacMan(pygame.sprite.Sprite):
     def __init__(self):
         super(PacMan, self).__init__()
 
+# Fiende classen
 class Ghost(pygame.sprite.Sprite):
     def __init__(self):
         super(Ghost, self).__init__()
@@ -34,9 +44,18 @@ Life = 3
 
 running = True
 
+path=[((0,400),(200,30)),((200,400),(30,200)),((200,600),(300,30)),((470,300),(30,300)),((500,300),(250,30)),((0,200),(200,30))]
+
+''' HOVED LOOPEN '''
 while running:
 
     for event in pygame.event.get():
 
         if event.type == QUIT:
             running = False
+
+    screen.fill(COLOR_RED)
+    for x in path:
+        pygame.draw.rect(screen,(255,255,255),x)
+
+    pygame.display.flip()
