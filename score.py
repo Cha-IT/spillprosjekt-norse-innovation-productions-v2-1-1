3145837a3341a991881603f5c674fc7927467e83
ArtonLong <artonlong@gmail.com>
2023-12-05 11:06:50 +0100

    score complete

--- score.py
+++ score.py
@@ -5,16 +5,15 @@ class Score(object):
         self.white = 255,255,255
         self.count = 0
         self.font = pygame.font.SysFont("comicsans",40, True , True)
-        self.text = self.font.render(str(self.count),1,self.white)
+        self.text = self.font.render('Voices gained :' + str(self.count),1,self.white)
 
     def show_score(self, screen, width):
-        screen.blit(self.text, (width/2 - 40, 0))
+        screen.blit(self.text, (width/2 - width/2, 0))
 
     def score_up(self):
         self.count += 1
-        self.text = self.font.render(str(self.count),1,self.white)
+        self.text = self.font.render('Voices gained :' + str(self.count),1,self.white)
 
 def score_instance(score_instance, screen, width):
     score_instance.score_up()
     score_instance.show_score(screen, width)
-    print(score_instance.count)
